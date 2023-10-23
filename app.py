1119e17c081c05ad4b03616da9e01f2c4d7851cd
unknown <rkaus52@naver.com>
2023-10-23 16:03:08 +0900

    스케줄 시간, 이벤트 창 전환 오류 해결, 테이블 탐색 순서 변경

---
+++ app.py
@@ -0,0 +1,257 @@
+from flask import Flask, request, jsonify
+import login_test
+from flask_cors import CORS
+import sqlite3
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+# db에 등록된 예약 정보를 확인 후 매크로 실행, 예약 정보 제거
+
+
+def check_and_delete_reservations():
+    try:
+        current_datetime = datetime.now()
+        print("Current Timestamp:", current_datetime)
+        reservation_data = get_reservation_data()
+        print(reservation_data)
+
+        # 예약 정보 테이블에서 매크로 시작 날짜와 현재 날짜가 동일한 정보가 있는지 확인
+        for row in reservation_data:
+            date_parts = "".join(row[3].split()[:-1])
+            year = date_parts.split('년')[0]
+
+            month = date_parts.split('년')[1].split('월')[0]
+
+            day = date_parts.split('년')[1].split('월')[1].split('일')[0]
+
+            date_set = year + '-' + month + '-' + day
+
+            selectedDay = datetime.strptime(date_set, '%Y-%m-%d')
+
+            print(type(selectedDay))
+            print(
+                f'current_datetime: {current_datetime}, selectedDay: {selectedDay}')
+
+            # 로그인 진행 시간을 08:59:30로 지정
+            # 테스트 시에는 and 이후 제거
+            # (current_datetime.day == selectedDay.day and current_datetime.hour == 8 and current_datetime.minute == 59 and current_datetime.second >= 30)  or (current_datetime.day == selectedDay.day and current_datetime.hour == 9 and current_datetime.minute <= 1)
+            if(current_datetime.day == selectedDay.day and current_datetime.hour == 9 and current_datetime.minute <= 1) or (current_datetime.day == selectedDay.day and current_datetime.hour == 8 and current_datetime.minute == 59 and current_datetime.second <= 59) or (current_datetime.day == selectedDay.day and current_datetime.hour == 10 and current_datetime.minute <= 1) or (current_datetime.day == selectedDay.day and current_datetime.hour == 9 and current_datetime.minute == 59 and current_datetime.second <= 59):
+                print('메크로 시작')
+                # 매크로 실행(로그인)
+                cookies, elapsed_time = login_test.login_test(
+                    "https://www.debeach.co.kr/",
+                    row[1], row[2], row[3], row[4], row[5], row[6], row[0], row[7]
+                )
+                # db에서 해당 예약 정보 제거
+                # if (current_datetime.day == selectedDay.day and current_datetime.hour == 10 and current_datetime.minute == 1):
+                #     print(current_datetime.hour)
+                #     print(current_datetime.minute)
+                #     delete_reservation_data(row[0])
+                return str(elapsed_time), 200
+
+    except Exception as e:
+        if (current_datetime.day == selectedDay.day and current_datetime.hour == 10 and current_datetime.minute == 1):
+            print(current_datetime.hour)
+            print(current_datetime.minute)
+            delete_reservation_data(row[0])
+        print('Error:', e)
+
+
+# CORS 에러 해결 코드
+app = Flask(__name__)
+CORS(app)
+
+
+# DB에 예약 정보 추가
+def insert_reservation_data(id, pw, selectedDay, nextFuture, futureTime, personnel, orderCheck):
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    query = """
+
+    INSERT INTO Reservation (uid, upw, selectedDay, nextFuture, futureTime, personnel, orderCheck)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
+
+    cursor.execute(query, (id, pw, selectedDay,
+                   nextFuture, futureTime, personnel, orderCheck))
+
+    conn.commit()
+    conn.close()
+
+
+# 전체 테이블 정보 조회
+def get_reservation_data():
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    query = """
+    SELECT * FROM Reservation
+    """
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    conn.close()
+    print(f'rows: {rows}')
+    return rows
+
+
+# 예약 정보 제거
+def delete_reservation_data(id):
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    query = """
+    DELETE FROM Reservation WHERE id=?
+    """
+
+    cursor.execute(query, (id,))
+
+    conn.commit()
+    conn.close()
+
+
+def get_reservation_result_data():
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    query = """
+    SELECT * FROM ResultLog
+    """
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    conn.close()
+    print(f'rows: {rows}')
+    return rows
+
+
+# 예약 등록 컨트롤러
+@app.route('/reservation', methods=['POST'])
+def reservation_route():
+    try:
+        print(request.form)
+        id = request.form.get('id')
+        pw = request.form.get('pw')
+        personnel = request.form.get('personnel')
+        selectedDay = request.form.get('selectedDay')
+        nextFuture = request.form.get('nextFuture')
+        futureTime = request.form.get('futureTime')
+        orderCheck = request.form.get('orderCheck')
+        insert_reservation_data(id, pw, selectedDay,
+                                nextFuture, futureTime, personnel, orderCheck)
+
+        return jsonify({'success': True}), 200
+
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
+
+
+# 예약 정보 조회 컨트롤러
+@app.route('/reservation_table', methods=['GET'])
+def reservation_table():
+    try:
+        reservation_data = get_reservation_data()
+
+        # db 테이블 정보를 json 형태로 프론트에 반환
+        reservations = []
+        for row in reservation_data:
+            reservation = {
+                'id': row[0],
+                'uid': row[1],
+                'upw': row[2],
+                'selectedDay': row[3],
+                'nextFuture': row[4],
+                'futureTime': row[5],
+                'personnel': row[6],
+                'orderCheck': row[7],
+            }
+            reservations.append(reservation)
+
+        return jsonify({'reservations': reservations}), 200
+
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
+
+
+# DB 예약 정보 제거 컨트롤러
+@app.route('/reservation_cancel/<int:id>', methods=['DELETE'])
+def reservation_cancel_route(id):
+    try:
+        delete_reservation_data(id)
+
+        return jsonify({'success': True}), 200
+
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
+
+
+@app.route('/reservation_result', methods=['GET'])
+def reservation_result():
+    try:
+        result_log_data = get_reservation_result_data()
+
+        # db 테이블 정보를 json 형태로 프론트에 반환
+        resultLogData = []
+        for row in result_log_data:
+            resultLogDataObj = {
+                'id': row[0],
+                'selectedDay': row[1],
+                'personnel': row[2],
+                'nextFuture': row[3],
+                'futureTime': row[4],
+                'result': row[5],
+                'course': row[6],
+                'teeUpTime': row[7]
+            }
+            resultLogData.append(resultLogDataObj)
+
+        return jsonify({'resultLogData': resultLogData}), 200
+
+    except Exception as e:
+        error_message = str(e)
+        response = {'success': False, 'error': error_message}
+        return jsonify(response), 500
+
+
+# # 스케줄러 적용
+scheduler = BackgroundScheduler(daemon=True)
+
+# # 배포용 코드: 59분 30초마다 매크로 실행 가능한 예약 정보를 탐색함.
+# # # 테스트용 코드: 50초가 될 때마다 스케줄링 
+# scheduler.add_job(check_and_delete_reservations,
+#                  'interval', seconds=50)
+# set_date_time = datetime.now()
+# if set_date_time.hour == 13:
+#     print("123")
+#     while(1):
+#         scheduler.add_job(check_and_delete_reservations,
+#                   trigger='cron', hour='*', minute='*', second='30')
+        
+#         time.sleep(30)
+#         if set_date_time.minute == 42:
+#             break
+scheduler.add_job(check_and_delete_reservations,
+                   trigger='cron', hour='*', minute='59', second='56') # 09:00:00 - 09:00:20
+scheduler.add_job(check_and_delete_reservations,
+                   trigger='cron', hour='*', minute='0', second='25') # 09:00:20 - 09:00:40
+scheduler.add_job(check_and_delete_reservations,
+                  trigger='cron', hour='*', minute='0', second='55') # 09:00:40 - 09:01:00
+scheduler.add_job(check_and_delete_reservations,
+                   trigger='cron', hour='*', minute='1', second='25') # 09:01:00 - 09:01:20
+scheduler.start()
+# scheduler.remove_all_jobs()
+# check_and_delete_reservations()
+
+if __name__ == '__main__':
+
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ reservation.py
@@ -0,0 +1,256 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from datetime import datetime
+from custom_exception import NoAvailableSlotsException
+from selenium.webdriver.common.alert import Alert
+import sqlite3
+import app
+
+
+
+
+def find_closest_time(bk_time_list, futureTime):  # 최적시간 찾기 메서드
+    # 조건: 지정시간 1시간 내외, 그 중 가장 가까운 시간 택 1
+    # 이전의 시간과 병행 지정할 경우, diff 연산을 abs로 처리
+    # 조건: 지정시간 2시간 내, 그 중 가장 가까운 시간 택 1
+    # abs삭제, diff <= 60 비교는 얼마 시간이 경과 후 인지 확인.
+    # (time - target_time).total_seconds() // 60 
+    target_time = datetime.strptime(futureTime, "%H:%M")
+    closest_time = None
+    min_diff = float('inf')
+
+    for time_str in bk_time_list:
+        time = datetime.strptime(time_str, "%H:%M")
+        if time >= target_time:
+            diff = (time - target_time).total_seconds() // 60     
+            
+            if diff <= 120 and diff < min_diff:
+                closest_time = time_str
+                min_diff = diff
+
+    return closest_time
+
+def find_farthest_time(bk_time_list, futureTime):  # 가장 먼 시간 찾기 메서드
+    # 조건: 지정시간 2시간 내, 그 중 가장 먼 시간 택 1
+    target_time = datetime.strptime(futureTime, "%H:%M")
+    farthest_time = None
+    max_diff = 0  # 초기값을 0으로 설정
+
+    for time_str in bk_time_list:
+        time = datetime.strptime(time_str, "%H:%M")
+        if time >= target_time:
+            diff = (time - target_time).total_seconds() // 60
+            if diff <= 120 and diff > max_diff:
+                farthest_time = time_str
+                max_diff = diff
+
+    return farthest_time
+
+# 예약 결과 로그 db에 기록
+def insert_result_log(selectedDay, personnel, nextFuture, futureTime, result, course, teeUpTime):
+    conn = sqlite3.connect("C:/golf_db/golf_db.db")
+    cursor = conn.cursor()
+
+    query = """
+
+    INSERT INTO ResultLog (selectedDay, personnel, nextFuture, futureTime, result, course, teeUpTime)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
+
+    cursor.execute(query, (selectedDay, personnel, nextFuture,
+                   futureTime, result, course, teeUpTime))
+
+    conn.commit()
+    conn.close()
+
+
+def reservation_test(driver, target_day, elements, target_month, futureTime, personnel, selectedDay, nextFuture, tid, order):
+    print("reservation_test module open!")
+    check_time = datetime.now()
+    for element in elements:
+
+        h2_elements = element.find_elements(By.TAG_NAME, 'h2')
+        for h2_element in h2_elements:
+            h2_text = h2_element.text
+
+            if target_month+'월' in h2_text:
+                print(h2_text)
+                table_element = h2_element.find_element(
+                    By.XPATH, './following-sibling::table')
+
+                td_elements = table_element.find_elements(
+                    By.XPATH, './/td[@class="  "]')
+                print('success to finding td_elements!')
+                for td_element in td_elements:
+                    a_elements = td_element.find_elements(
+                        By.TAG_NAME, 'a')
+                    for a_element in a_elements:
+                        a_text = a_element.text
+
+                        if str(target_day) in a_text:
+                            print('trying to find table..')
+                            try:
+                                td_element.click()
+                            except Exception as error:
+                                # if isWednesday:
+                                #     print('exception 테스트-------------------')
+                                #     closeBtn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+                                #         (By.XPATH, '//*[@id="modal-view"]/div/div/div[1]/button/span/i')))
+                                #     closeBtn.click()
+                                #     return
+                                print(error)
+                                insert_result_log(
+                                    selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                                driver.close()
+                                return error, 500
+
+                            print('successed to finding reservable day!')
+                            elements = driver.find_element(
+                                By.CLASS_NAME, 'col-xs-7')
+
+                           # 첫번째 예약 버튼 로드까지 기다림
+                            try:
+                                tr_elements_load = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+                                    (By.XPATH, '//*[@id="booking-index"]/div[2]/div[2]/div/table/tbody/tr[1]/td[5]/button[1]')))
+                            except Exception as error:
+                                # if isWednesday:
+                                #     print('exception 테스트-------------------')
+                                #     closeBtn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+                                #         (By.XPATH, '//*[@id="modal-view"]/div/div/div[1]/button/span/i')))
+                                #     closeBtn.click()
+                                #     return
+                                print(error)
+                                insert_result_log(
+                                    selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                                driver.close()
+                                return error, 500
+
+                            time_tbody = elements.find_element(
+                                By.XPATH, '//*[@id="booking-index"]/div[2]/div[2]/div/table/tbody')
+                            tr_elements = time_tbody.find_elements(
+                                By.TAG_NAME, "tr")
+                            bk_time_list = []
+                            td_elements_list = []
+
+                            for tr in tr_elements:
+                                td_elements = tr.find_elements(
+                                    By.CLASS_NAME, "bk_time")
+                                td_elements_list.extend(td_elements)
+                                for td in td_elements:
+                                    strong_element = td.find_element(
+                                        By.TAG_NAME, "strong")
+                                    bk_time_list.append(strong_element.text)
+
+                            print("bk_time_list:", bk_time_list)
+                            if order == '1':
+                                closest_time = find_closest_time(
+                                    bk_time_list, futureTime)
+                                print(order + "가장 가까운 것")
+                            else:
+                                closest_time = find_farthest_time(
+                                    bk_time_list, futureTime)
+                                print(order + "가장 먼 것")
+                            if closest_time is not None:
+                                print("Closest time:", closest_time)
+                            else:
+                                print("There is no available time slot.")
+                                insert_result_log(
+                                    selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                                return
+
+                            # 가장 가까운 시간의 tr요소에서 예약 버튼 찾아서 클릭
+                            for tr in tr_elements:
+                                td_element = tr.find_element(
+                                    By.CLASS_NAME, "bk_time")
+                                strong_element = td_element.find_element(
+                                    By.TAG_NAME, "strong")
+                                closest_time = str(closest_time)
+                                if closest_time == strong_element.text:
+                                    print('find matched value!')
+                                    reservation_btn = tr.find_elements(
+                                        By.TAG_NAME, "button")[0]
+
+                                    course = tr.find_element(
+                                        By.CLASS_NAME, "bk_cours").text
+                                    print('course: ' + course)
+                                    reservation_btn.click()
+
+                                    # 인원 수 체크
+                                    radio_element = WebDriverWait(driver, 5).until(
+                                        EC.element_to_be_clickable((By.XPATH, '//*[@id="form-create"]/div[6]/div')))
+                                    print(radio_element)
+                                    if personnel == '3':
+                                        cnt3 = radio_element.find_element(
+                                            By.XPATH, '//*[@id="form-create"]/div[6]/div/label[1]')
+                                        cnt3.click()
+                                    else:
+                                        cnt4 = radio_element.find_element(
+                                            By.XPATH, '//*[@id="form-create"]/div[6]/div/label[2]')
+                                        cnt4.click()
+
+                                    submit_btn = driver.find_element(
+                                        By.XPATH, '//*[@id="form-create"]/div[9]/button[1]')
+                                    submit_btn.click()
+
+                                    # 모달창이 나타나는 부분
+                                    try:
+                                        alert1 = WebDriverWait(driver, 5).until(
+                                            EC.alert_is_present())
+
+                                        alert1.accept()
+                                        print("모달창 확인 버튼 클릭 완료!")
+
+                                        try:
+
+                                            print("예약 완료 테이블 탐색중...")
+                                            reservation_result_table = WebDriverWait(driver, 5).until(
+                                                EC.presence_of_element_located(
+                                                    (By.XPATH, '//*[@id="booking-history"]/table[1]'))
+                                            )
+                                            print("예약 완료!")
+                                            print(check_time)
+                                            insert_result_log(
+                                                selectedDay, personnel, nextFuture, futureTime, '성공', course, closest_time)
+                                            reservation_data = app.get_reservation_data()
+                                            app.delete_reservation_data(tid)
+                                            return "예약 성공"
+
+                                        except Exception as error:
+                                            # if isWednesday:
+                                            #     closeBtn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+                                            #         (By.XPATH, '//*[@id="modal-view"]/div/div/div[1]/button/span/i')))
+                                            #     closeBtn.click()
+                                            #     return
+                                            print(error)
+                                            insert_result_log(
+                                                selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                                            driver.close()
+                                            return error, 500
+
+                                    except Exception as error:
+                                        # if isWednesday:
+                                        #     print(
+                                        #         'exception 테스트-------------------')
+                                        #     return
+                                        print(error)
+                                        insert_result_log(
+                                            selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                                        driver.close()
+                                        return error, 500
+
+                else:
+                    print('cannot find matched a_text.. ㅜ.ㅜ')
+                    # if isWednesday:
+                    #     print('exception 테스트-------------------')
+                    #     return
+                    # else:
+                    insert_result_log(
+                        selectedDay, personnel, nextFuture, futureTime, '실패', 'X', 'X')
+                    driver.close()
+                    raise NoAvailableSlotsException(
+                        '해당 날짜에 예약 가능한 시간대가 없습니다.')
+
+        # if target_month in h2_text:
+        #     break
